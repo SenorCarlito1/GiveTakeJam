8c2db8bd32337f94fe244d201bc251b3d7724373
Carlos Ocampo <118745644+SenorCarlito1@users.noreply.github.com>
2024-06-20 19:18:44 -0400

    Added Rotation to Player Build mode

--- Assets/Scripts/Player/BuildingPlacement.cs
+++ Assets/Scripts/Player/BuildingPlacement.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class BuildingPlacement : MonoBehaviour
 {
     [SerializeField] private float rayDistance;
-    [SerializeField] private LayerMask buildModLayerMask;
     [SerializeField] private int defaultLayerInt;
+    [SerializeField] private LayerMask buildModLayerMask;
     [SerializeField] private Transform rayOrigin;
     [SerializeField] private Material buildingMatPositive;
     [SerializeField] private Material buildingMatNegative;
@@ -17,39 +18,61 @@ public class BuildingPlacement : MonoBehaviour
     private Camera _camera;
 
     public GameObject gameObjectToPosition;
+
     void Start()
     {
-        _camera = Camera.main;
+        _camera = GameManager.instance.currCamera;
     }
 
     // Update is called once per frame
     void Update()
     {
+        _camera = GameManager.instance.currCamera;
+
         if (gameObjectToPosition == null || !IsRayHit(buildModLayerMask, out RaycastHit hitInfo))
         {
             return;
         }
-
+        
+        // Changes 
         if (Input.GetKeyDown(KeyCode.F6)) inBuildMode = true;
 
-        
-        if (inBuildMode)
+        if (inBuildMode && GameManager.instance.currCamera)
         {
             gameObjectToPosition.transform.position = hitInfo.point + new Vector3(0, 3, 0);
-
+            if (Input.GetKey(KeyCode.R))
+            {
+                gameObjectToPosition.transform.Rotate(0, 1, 0);
+            }
             if (Input.GetKeyDown(KeyCode.E))
             {
-                // transform.position + (transform.forward * 10) + new Vector3(0, 2, 0)
-                Instantiate(gameObjectToPosition, hitInfo.point + new Vector3(0, 3, 0), Quaternion.identity);
+
+                Instantiate(gameObjectToPosition, hitInfo.point + new Vector3(0, 3, 0), gameObjectToPosition.transform.rotation);
                 inBuildMode = false;
             }
         }
-        
+
 
 
 
     }
+    private void FixedUpdate()
+    {
+        // Show the raycast line in the scene window
 
+        RaycastHit hit;
+        if (Physics.Raycast(_camera.transform.position, _camera.transform.forward, out hit, Mathf.Infinity))
+        {
+
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
     private bool IsRayHit(LayerMask layerMask, out RaycastHit hitInfo)
     {
         var ray = new Ray(rayOrigin.position, _camera.transform.forward * rayDistance);
